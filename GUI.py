5bf7af3e269cc63fb0bcc212a2fc9975d504883a
TwoToed <PeranutYl@gmail.com>
2024-05-03 15:02:05 +0700

    third commit

--- GUI.py
+++ GUI.py
@@ -1,19 +1,28 @@
 import tkinter
 import detection
 from datetime import datetime
+import time
 
-start_time = datetime.now().strftime("%H:%M:%S")
+def start_clicked():
+    print("hello")
+    detection.set_time_track(time.time())
+    detection.process_video()
 def create_gui():
+    def update_labels():
+        at_com_label.config(text="at com time in min: " + "{:.2f}".format(detection.get_at_com() / 60))
+        not_at_com_label.config(text="not at com time in min: " + "{:.2f}".format(detection.get_not_at_com() / 60))
 
-    def start_clicked():
-        print("hello")
-        detection.process_video(detection.video_capture)
     root = tkinter.Tk()
     root.title("Sit Still")
     root.geometry('500x500')
-    startlabel = tkinter.Label(root, text = "Start Time: " + start_time, fg = "red")
+    #labels
     startbtn = tkinter.Button(root, text="Start Video Capture", fg="red", command=start_clicked)
+    updatebtn = tkinter.Button(root, text="Check Timing", fg="red", command=update_labels)
+    at_com_label = tkinter.Label(root, text="at com time in min: 0")
+    not_at_com_label = tkinter.Label(root, text="not at com time in min: 0")
     startbtn.grid(column=0, row=0)
-    startlabel.grid(column=0, row=1)
+    updatebtn.grid(column=0, row=1)
+    at_com_label.grid(column=0, row=2)
+    not_at_com_label.grid(column=0, row=3)
     root.mainloop()
 
--- detection.py
+++ detection.py
@@ -1,12 +1,19 @@
 import cv2
 import time
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-video_capture = cv2.VideoCapture(0)
+
 at_com = 0
 not_at_com = 0
-
+time_track = 0
+def get_at_com():
+    return at_com
+def get_not_at_com():
+    return not_at_com
+def set_time_track(start_time):
+    global time_track
+    time_track = start_time
 def detect_bounding_box(vid):
-    time_track = time.time()
+    global time_track
     global at_com
     global not_at_com
     #gray_image converts color frame to gray for better detection
@@ -14,22 +21,22 @@ def detect_bounding_box(vid):
     #limit to one face
     face = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))[:1]
     print(face)
+    cur_time = time.time()
     if len(face) == 0:
-        cur_time = time.time()
-        at_com += cur_time - time_track
-        time_track = cur_time
-        print("at com" + str(at_com))
-    else:
-        #only has one face
-        x, y, w, h = face[0]
-        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        cur_time = time.time()
         not_at_com += cur_time - time_track
         time_track = cur_time
         print("not at com" + str(not_at_com))
+    else:
+        at_com += cur_time - time_track
+        time_track = cur_time
+        print("at com" + str(at_com))
+        #only detect one face at a time
+        x, y, w, h = face[0]
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
     return face
 
-def process_video(video_capture):
+def process_video():
+    video_capture = cv2.VideoCapture(0)
     if not video_capture.isOpened():
         print("error: camera not turning on")
     while True:
@@ -47,6 +54,6 @@ def process_video(video_capture):
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-
+    
     video_capture.release()
     cv2.destroyAllWindows()
